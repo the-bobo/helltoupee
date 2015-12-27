2c7e59b7b90be1482f3ccf1633c6ce606ca9af51
Bobo <the.bobo@gmail.com>
2015-12-27 13:11:46 -0500

    made http.createServer fire every 60s to avoid heroku spinning down dyno

--- helltoupee.js
+++ helltoupee.js
@@ -17,10 +17,10 @@ var http = require('http');
 ******************************************************
 */
 
-http.createServer(function (req, res) { 
+setInterval(http.createServer(function (req, res) { 
   res.writeHead(200, {'Content-Type': 'text/plain'}); 
   res.send('it is running\n'); 
-}).listen(process.env.PORT || 5000);
+}).listen(process.env.PORT || 5000), 60000);
 
 /*
 http.createServer(function (request, response) {
